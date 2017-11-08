c16d84889477e09f4ee5197cf4ba974829d4c65a
chrivand <chrivand@cisco.com>
2017-11-08 14:29:28 +0100

    changed checkbocked to Ad and Umbrella
    no Add and OPDS

--- CheckBlocked.py
+++ CheckBlocked.py
@@ -3,8 +3,8 @@ import requests
 from config import Config
 
 l_odns =[] 
-l_add  =[]
-f = file('AddBlocker.cfg')
+l_ads  =[]
+f = file('AdBlocker.cfg')
 cfg = Config(f)
 # Create a list of all urls in Opendns integration
 Url = cfg.domainurl+'?customerKey='+cfg.custkey
@@ -24,12 +24,12 @@ Url = cfg.addurl
 r = requests.get(Url)
 for line in r.iter_lines() :
     if (line[0:1] is '0') :
-        l_add.append(line[8:])
-l_add_js = frozenset(json.loads(json.dumps(l_add)))
-l_odns_js = frozenset(json.loads(json.dumps(l_odns)))
-dom_remove = l_odns_js - l_add_js
-dom_add = l_add_js - l_odns_js
-print 'domains in addlist ' ,len(l_add_js)
-print 'domains being blocked ',len(l_odns_js)
-print 'domains to be removed from odns ',len(dom_remove)
-print 'domains to be added to odns ' , len(dom_add)
+        l_ads.append(line[8:])
+l_ads_js = frozenset(json.loads(json.dumps(l_ads)))
+l_umbrella_js = frozenset(json.loads(json.dumps(l_odns)))
+dom_remove = l_umbrella_js - l_ads_js
+dom_add = l_ads_js - l_umbrella_js
+print 'domains in addlist ' ,len(l_ads_js)
+print 'domains being blocked ',len(l_umbrella_js)
+print 'domains to be removed from umbrella ',len(dom_remove)
+print 'domains to be added to umbrella ' , len(dom_add)
